8e0c3435017ed637701cb8a125b7a89bb5d1154d
Mat Trudel <mat@geeky.net>
2014-03-06 11:32:27 -0500

    Add public_hostname to nginx config, remove default host

--- examples/unicorn/config/deploy/production.rb
+++ examples/unicorn/config/deploy/production.rb
@@ -2,4 +2,6 @@ set :stage, :production
 
 set :rails_env, 'production'
 
+set :public_hostname, 'example.com'
+
 server 'example.com', user: 'deploy', roles: %w{web app db}
--- examples/unicorn/config/deploy/vagrant.rb
+++ examples/unicorn/config/deploy/vagrant.rb
@@ -4,3 +4,5 @@ set :rails_env, 'development'
 set :bundle_without, 'production'
 
 server '192.168.50.4', user: 'deploy', roles: %w{web app}
+set :public_hostname, '192.168.50.4'
+
