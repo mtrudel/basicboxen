6dea4d2a5b6f4eafc17c9c0b2fc88dd8bd8c30c1
Mat Trudel <mat@geeky.net>
2014-06-02 10:37:36 -0400

    Remove patch now that capistrano/sshkit#144 has landed

--- examples/unicorn/config/deploy.rb
+++ examples/unicorn/config/deploy.rb
@@ -46,9 +46,7 @@ namespace :deploy do
     on roles(:app) do |host|
       within current_path do
         as :root do
-          # rbenv/shims isn't in root's path, so we need to explicitly path it
-          # since command maps are wonky within as() blocks
-          execute "#{fetch(:rbenv_custom_path)}/shims/bundle", :exec, :foreman, :export, :upstart, :'-f', 'Procfile', :'-a', fetch(:application), :'-d', current_path, :'-u', host.user, :'/etc/init'
+          execute :bundle, :exec, :foreman, :export, :upstart, :'-f', 'Procfile', :'-a', fetch(:application), :'-d', current_path, :'-u', host.user, :'/etc/init'
         end
       end
     end
