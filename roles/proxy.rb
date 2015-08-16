b9894a6db398fb700b91d3282ed1788b31d5b551
@anulman <anulman@gmail.com>
2015-08-15 20:53:44 -0400

    update default openssl version for nginx

--- roles/proxy.rb
+++ roles/proxy.rb
@@ -5,11 +5,12 @@ run_list(
   "recipe[nginx]",
 )
 
+# define openssl_source below because the recipe defaults to an earlier version
 override_attributes(
   "nginx" => {
     "default_site_enabled" => false,
     "openssl_source" => {
-      "version" => "1.0.1h"
+      "version" => "1.0.2d"
     }
   }
 )
