436104055cb9ff0b9cf9fec9bb3ce764f157ba4e
@anulman <anulman@gmail.com>
2016-11-22 17:30:16 -0500

    feat(ruby-app-role): rm install_libv8_dev by default as it's unnecessary for api builds

--- roles/ruby_app.rb
+++ roles/ruby_app.rb
@@ -5,6 +5,5 @@ run_list(
   "recipe[build-essential]",
   "recipe[rbenv]",
   "recipe[rbenv::ruby_build]",
-  "recipe[site-ruby::install_ruby]",
-  "recipe[site-libv8::install_libv8_dev]",
+  "recipe[site-ruby::install_ruby]"
 )
