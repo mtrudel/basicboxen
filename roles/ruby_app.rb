d4a99ea3eefcdeaa328318fc8e5a2c7e686e6229
@anulman <anulman@gmail.com>
2016-12-23 03:08:08 -0500

    fix(rbenv): add support for rbenv env vars
    # Conflicts:
    #	roles/ruby_app.rb

--- roles/ruby_app.rb
+++ roles/ruby_app.rb
@@ -5,5 +5,7 @@ run_list(
   "recipe[build-essential]",
   "recipe[rbenv]",
   "recipe[rbenv::ruby_build]",
-  "recipe[site-ruby::install_ruby]"
+  "recipe[site-ruby::install_ruby]",
+  "recipe[site-libv8::install_libv8_dev]",
+  "recipe[rbenv::rbenv_vars]"
 )
